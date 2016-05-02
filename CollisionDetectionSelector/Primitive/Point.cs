ce7b44078c8920be747825579514a35ed3f53b80
mszauer <mszauer@mail.usf.edu>
2016-05-02 14:50:29 -0400

    finished point, error when running

--- CollisionDetectionSelector/Primitive/Point.cs
+++ CollisionDetectionSelector/Primitive/Point.cs
@@ -1,35 +1,67 @@
 ﻿using Math_Implementation;
 using OpenTK.Graphics.OpenGL;
 
-class Point {
-    protected Vector3 position = new Vector3();
-    public Vector3 Position {
-        get;//todo
-        set;//todo
+namespace CollisionDetectionSelector.Primitive {
+    class Point {
+        protected Vector3 position = new Vector3();
+        public Vector3 Position {
+            get {
+                return position;
+            }
+            set {
+                if (position == null) {
+                    position = new Vector3();
+                }
+                position.X = value.X;
+                position.Y = value.Y;
+                position.Z = value.Z;
+            }
+        }
+        public float X {
+            get {
+                return position.X;
+            }
+            set {
+                position.X = value;
+            }
+        }
+        public float Y {
+            get {
+                return position.Y;
+            }
+            set {
+                position.Y = value;
+            }
+        }
+        public float Z {
+            get {
+                return position.Z;
+            }
+            set {
+                position.Z = value;
+            }
+        }
+        public Point() {
+            Position = new Vector3();
+        }
+        public Point(float x, float y, float z) {
+            Position = new Vector3(x, y, z);
+        }
+        public Point(Vector3 v) {
+            Position = new Vector3(v.X, v.Y, v.Z);
+        }
+        public void FromVector(Vector3 v) {
+            Position = new Vector3(v.X, v.Y, v.Z);
+        }
+        #region Rendering
+        public void Render() {
+            GL.Begin(PrimitiveType.Points);
+            GL.Vertex3(Position.X, Position.Y, Position.Z);
+            GL.End();
+        }
+        public override string ToString() {
+            return "(" + Position.X + ", " + Position.Y + ", " + Position.Z + ")";
+        }
+        #endregion
     }
-    public float X { get; set; }//todo
-    public float Y { get; set; }//todo
-    public float Z { get; set; }//todo
-    public Point() {
-        //todo
-    }
-    public Point(float x,float y,float z) {
-        //todo
-    }
-    public Point(Vector3 v) {
-        //todo make new
-    }
-    public void FromVector(Vector3 v) {
-        //todo make new
-    }
-    #region Rendering
-    public void Render() {
-        GL.Begin(PrimitiveType.Points);
-        GL.Vertex3(Position.X, Position.Y, Position.Z);
-        GL.End();
-    }
-    public override string ToString() {
-        return "(" + Position.X + ", " + Position.Y + ", " + Position.Z + ")";
-    }
-    #endregion
 }
--- CollisionDetectionSelector/Samples/PointSample.cs
+++ CollisionDetectionSelector/Samples/PointSample.cs
@@ -1,4 +1,36 @@
 ﻿using OpenTK.Graphics.OpenGL;
 using Math_Implementation;
-using CollisionDetectionSelector;
+using CollisionDetectionSelector.Primitive;
 
+namespace CollisionDetectionSelector.Samples {
+    class PointSample : Application {
+        Point[] points = null;
+
+        public override void Initialize(int width, int height) {
+            points = new Point[100];
+            for (int i = 0; i < 100; ++i) {
+                points[i] = new Point(i - 50, i - 50, i - 50);
+            }
+            GL.PointSize(2f);
+        }
+
+        public override void Render() {
+            Matrix4 lookAt = Matrix4.LookAt(new Vector3(0.0f, 0.0f, 30.0f), new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 1.0f, 0.0f));
+            GL.LoadMatrix(lookAt.OpenGL);
+
+            for (int i = 0; i < points.Length; ++i) {
+                points[i].Render();
+            }
+        }
+
+        public override void Resize(int width, int height) {
+            GL.Viewport(0, 0, width, height);
+            GL.MatrixMode(MatrixMode.Projection);
+            float aspect = (float)width / (float)height;
+            Matrix4 perspective = Matrix4.Perspective(60, aspect, 0.01f, 1000.0f);
+            GL.LoadMatrix(Matrix4.Transpose(perspective).Matrix);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+        }
+    }
+}
