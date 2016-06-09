431b2c0202874b1002a7d60fd8985b5de42661b2
mszauer <mszauer@mail.usf.edu>
2016-06-09 19:02:29 -0400

    i think closest point got the furthest point?

--- CollisionDetectionSelector/Collisions/LineCollisions.cs
+++ CollisionDetectionSelector/Collisions/LineCollisions.cs
@@ -4,6 +4,16 @@ using CollisionDetectionSelector.Primitive;
 
 namespace CollisionDetectionSelector.Collisions {
     class LineCollisions {
+        public static T Clamp<T> (T value, T max, T min) {
+            T result = value;
+            if ((float)System.Convert.ToInt32(result) > (float)System.Convert.ToInt32(max)) {
+                result = max;
+            }
+            else if ((float)System.Convert.ToInt32(result) < (float)System.Convert.ToInt32(min)) {
+                result = min;
+            }
+            return result;
+        }
         public static bool PointOnLine(Point p,Line line) {
             float m = (line.End.Y - line.Start.Y) / (line.End.X - line.Start.X);//rise over run
             float b = line.Start.Y - m * line.Start.X;
@@ -16,6 +26,29 @@ namespace CollisionDetectionSelector.Collisions {
                 return true;
             }
             return false;
+        }
+        public static Point ClosestPoint(Line ab,Point c,out float t) {
+            Vector3 a = new Vector3(ab.Start.X, ab.Start.Y, ab.Start.Z);
+            Vector3 b = new Vector3(ab.End.X, ab.End.Y, ab.End.Z);
+
+            //project c onto b, then find the
+            //parametrized position d(t) = a+t*(b-a)
+            //t = Dot(c - a, ab) / Dot(ab, ab);
+            t = Vector3.Dot(c.ToVector() - a, ab.ToVector())/Vector3.Dot(ab.ToVector(),ab.ToVector());
+           
+            //clamp t to 0-1 range
+            //if t is outside range, it is outside line
+            t = Clamp<float>(t, 0f, 1f);
+
+            //compute project position from clamped t
+            Point d = new Point(a + (ab.ToVector()*t));
+
+            //return result
+            return d;
+        }
+        public static Point ClosestPoint(Line ab,Point c) {
+            float t = 0f;
+            return ClosestPoint(ab, c, out t);
         }
     }
 }
---
+++ CollisionDetectionSelector/Samples/ClosestPointOnLineSample.cs
@@ -0,0 +1,88 @@
+﻿using OpenTK.Graphics.OpenGL;
+using Math_Implementation;
+using CollisionDetectionSelector.Primitive;
+
+namespace CollisionDetectionSelector.Samples {
+    class ClosestPointLine : Application {
+        protected Vector3 cameraAngle = new Vector3(120.0f, -10f, 20.0f);
+        protected float rads = (float)(System.Math.PI / 180.0f);
+
+        Line testLine = new Line(new Point(-3, -2, -1), new Point(1, 2, 3));
+        Point[] testPoints = new Point[] {
+            new Point(7, -2, -1),
+            new Point(-4, -7, -8),
+            new Point(7, 8, 5),
+            new Point(1, 5, -5),
+            new Point(-6, 5, 7),
+            new Point(1, 6, 8),
+            new Point(-7, -10, -4),
+            new Point(5, 5, 3)
+        };
+
+        public override void Initialize(int width, int height) {
+            GL.Enable(EnableCap.DepthTest);
+            GL.PointSize(4f);
+        }
+
+        public override void Render() {
+            Vector3 eyePos = new Vector3();
+            eyePos.X = cameraAngle.Z * -(float)System.Math.Sin(cameraAngle.X * rads * (float)System.Math.Cos(cameraAngle.Y * rads));
+            eyePos.Y = cameraAngle.Z * -(float)System.Math.Sin(cameraAngle.Y * rads);
+            eyePos.Z = -cameraAngle.Z * (float)System.Math.Cos(cameraAngle.X * rads * (float)System.Math.Cos(cameraAngle.Y * rads));
+
+            Matrix4 lookAt = Matrix4.LookAt(eyePos, new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 1.0f, 0.0f));
+            GL.LoadMatrix(Matrix4.Transpose(lookAt).Matrix);
+
+            DrawOrigin();
+
+            GL.Color3(1f, 0f, 1f);
+            testLine.Render();
+
+            GL.Color3(0f, 1f, 1f);
+            foreach (Point point in testPoints) {
+                point.Render();
+            }
+
+            GL.Color3(1f, 1f, 0f);
+            foreach (Point point in testPoints) {
+                Point closest = Collisions.LineCollisions.ClosestPoint(testLine, point);
+                closest.Render();
+            }
+
+            GL.Color3(1f, 1f, 1f);
+            foreach (Point point in testPoints) {
+                Point closest = Collisions.LineCollisions.ClosestPoint(testLine, point);
+                Line newLine = new Line(closest, point);
+                newLine.Render();
+            }
+        }
+
+        public override void Update(float deltaTime) {
+            cameraAngle.X += 45.0f * deltaTime;
+        }
+
+        protected void DrawOrigin() {
+            GL.Begin(PrimitiveType.Lines);
+            GL.Color3(1f, 0f, 0f);
+            GL.Vertex3(0f, 0f, 0f);
+            GL.Vertex3(1f, 0f, 0f);
+            GL.Color3(0f, 1f, 0f);
+            GL.Vertex3(0f, 0f, 0f);
+            GL.Vertex3(0f, 1f, 0f);
+            GL.Color3(0f, 0f, 1f);
+            GL.Vertex3(0f, 0f, 0f);
+            GL.Vertex3(0f, 0f, 1f);
+            GL.End();
+        }
+
+        public override void Resize(int width, int height) {
+            GL.Viewport(0, 0, width, height);
+            GL.MatrixMode(MatrixMode.Projection);
+            float aspect = (float)width / (float)height;
+            Matrix4 perspective = Matrix4.Perspective(60, aspect, 0.01f, 1000.0f);
+            GL.LoadMatrix(Matrix4.Transpose(perspective).Matrix);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+        }
+    }
+}
