172d109f2c53ef9ce2bf9ff29c08880ac1eb961c
mszauer <mszauer@mail.usf.edu>
2016-06-09 17:39:58 -0400

    pointonline is complete

--- CollisionDetectionSelector/Collisions/LineCollisions.cs
+++ CollisionDetectionSelector/Collisions/LineCollisions.cs
@@ -4,6 +4,18 @@ using CollisionDetectionSelector.Primitive;
 
 namespace CollisionDetectionSelector.Collisions {
     class LineCollisions {
-        
+        public static bool PointOnLine(Point p,Line line) {
+            float m = (line.End.Y - line.Start.Y) / (line.End.X - line.Start.X);//rise over run
+            float b = line.Start.Y - m * line.Start.X;
+
+            //at this point, evaluation equation is:
+            //return point.y==m*p.x+b
+            //wont work because we use floats
+            //floating error can be accumulated, episilon testing
+            if (Math.Abs(p.Y-(m*p.X+b)) < 0.0001f) {
+                return true;
+            }
+            return false;
+        }
     }
 }
---
+++ CollisionDetectionSelector/Samples/PointOnLineSample.cs
@@ -0,0 +1,81 @@
+﻿using OpenTK.Graphics.OpenGL;
+using Math_Implementation;
+using CollisionDetectionSelector.Primitive;
+
+namespace CollisionDetectionSelector.Samples {
+    class PointOnLineSample : Application {
+        protected Vector3 cameraAngle = new Vector3(120.0f, -10f, 20.0f);
+        protected float rads = (float)(System.Math.PI / 180.0f);
+
+        Line testLine = new Line(new Point(-3, -2, -1), new Point(3, 2, 1));
+        Point[] testPoints = new Point[] {
+            new Point(0, 0, 0),
+            new Point(-3, -2, -1),
+            new Point(3, 2, 1),
+            new Point(7, -2, -1),
+            new Point(-4, -7, -8),
+            new Point(7, 8, 5),
+            new Point(1, 5, -5),
+            new Point(-6, 5, 7),
+            new Point(1, 6, 8),
+            new Point(-7, -10, -4),
+            new Point(5, 5, 3)
+        };
+
+        public override void Initialize(int width, int height) {
+            GL.Enable(EnableCap.DepthTest);
+            GL.PointSize(4f);
+
+            for (int i = 0; i < 3; ++i) {
+                if (!Collisions.LineCollisions.PointOnLine(testPoints[i], testLine)) {
+                    System.Console.ForegroundColor = System.ConsoleColor.Red;
+                    System.Console.WriteLine("Expected point: " + testPoints[i].ToString() + " to be on Line!");
+                }
+            }
+
+            for (int i = 3; i < testPoints.Length; ++i) {
+                if (Collisions.LineCollisions.PointOnLine(testPoints[i], testLine)) {
+                    System.Console.ForegroundColor = System.ConsoleColor.Red;
+                    System.Console.WriteLine("Expected point: " + testPoints[i].ToString() + " to be on Line!");
+                }
+            }
+        }
+
+        public override void Render() {
+            Vector3 eyePos = new Vector3();
+            eyePos.X = cameraAngle.Z * -(float)System.Math.Sin(cameraAngle.X * rads * (float)System.Math.Cos(cameraAngle.Y * rads));
+            eyePos.Y = cameraAngle.Z * -(float)System.Math.Sin(cameraAngle.Y * rads);
+            eyePos.Z = -cameraAngle.Z * (float)System.Math.Cos(cameraAngle.X * rads * (float)System.Math.Cos(cameraAngle.Y * rads));
+
+            Matrix4 lookAt = Matrix4.LookAt(eyePos, new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 1.0f, 0.0f));
+            GL.LoadMatrix(Matrix4.Transpose(lookAt).Matrix);
+
+            GL.Color3(1f, 0f, 1f);
+            testLine.Render();
+
+            foreach (Point point in testPoints) {
+                if (Collisions.LineCollisions.PointOnLine(point, testLine)) {
+                    GL.Color3(1f, 0f, 0f);
+                }
+                else {
+                    GL.Color3(0f, 0f, 1f);
+                }
+                point.Render();
+            }
+        }
+
+        public override void Update(float deltaTime) {
+            cameraAngle.X += 45.0f * deltaTime;
+        }
+
+        public override void Resize(int width, int height) {
+            GL.Viewport(0, 0, width, height);
+            GL.MatrixMode(MatrixMode.Projection);
+            float aspect = (float)width / (float)height;
+            Matrix4 perspective = Matrix4.Perspective(60, aspect, 0.01f, 1000.0f);
+            GL.LoadMatrix(Matrix4.Transpose(perspective).Matrix);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+        }
+    }
+}
