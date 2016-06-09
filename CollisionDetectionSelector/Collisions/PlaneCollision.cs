bc854d002b851a4726a0f9376de6e928831f9dc9
mszauer <mszauer@mail.usf.edu>
2016-06-09 12:09:59 -0400

    added closest point on plane, not sure if fully correct via visual guide

--- CollisionDetectionSelector/Collisions/PlaneCollision.cs
+++ CollisionDetectionSelector/Collisions/PlaneCollision.cs
@@ -11,11 +11,22 @@ namespace CollisionDetectionSelector.Collisions {
             float result = Vector3.Dot((new Vector3(unknown.X, unknown.Y, unknown.Z)), p.Normal) - p.Distance;
             return Math.Abs(0f - result) < 0.00001f; //essentially 0
         }
-        public static float DistanceFromPlane(Point point, Plane plane) {
-            float dist = 0f;
-            //Vector3.Dot(point, normal(of plane)) = distance
-            dist = Vector3.Dot(new Vector3(point.X, point.Y, point.Z), plane.Normal);
-            return dist;
+        public static float DistanceFromPlane(Point unknown, Plane p) {
+            //Vector3.Dot(normal,unknown) = plane.distance
+            float result = Vector3.Dot((new Vector3(unknown.X, unknown.Y, unknown.Z)), p.Normal) - p.Distance;
+            return result;
+        }
+        public static Point ClosestPoint(Plane p, Point unknown,bool normalized = true) {
+            float distance = Vector3.Dot((new Vector3(unknown.X, unknown.Y, unknown.Z)), p.Normal) - p.Distance;
+            if (!normalized) {
+                distance = distance / (Vector3.Dot(p.Normal, p.Normal));
+            }
+            Point result = new Point(p.Normal * distance);
+            unknown.X -= result.X;
+            unknown.Y -= result.Y;
+            unknown.Z -= result.Z;
+            return unknown;
+
         }
     }
 }
---
+++ CollisionDetectionSelector/Samples/ClosesPointOnPlane.cs
@@ -0,0 +1,83 @@
+﻿using OpenTK.Graphics.OpenGL;
+using Math_Implementation;
+using CollisionDetectionSelector.Primitive;
+
+namespace CollisionDetectionSelector.Samples {
+    class ClosestPointPlaneSample : Application {
+        protected Vector3 cameraAngle = new Vector3(120.0f, -10f, 20.0f);
+        protected float rads = (float)(System.Math.PI / 180.0f);
+
+        Plane plane = new Plane(new Point(5, 6, 7), new Point(6, 5, 4), new Point(1, 2, 3));
+
+        Point point = new Point(2f, 5f, -3f);
+
+        public override void Initialize(int width, int height) {
+            GL.PointSize(2f);
+        }
+
+        public override void Render() {
+            Vector3 eyePos = new Vector3();
+            eyePos.X = cameraAngle.Z * -(float)System.Math.Sin(cameraAngle.X * rads * (float)System.Math.Cos(cameraAngle.Y * rads));
+            eyePos.Y = cameraAngle.Z * -(float)System.Math.Sin(cameraAngle.Y * rads);
+            eyePos.Z = -cameraAngle.Z * (float)System.Math.Cos(cameraAngle.X * rads * (float)System.Math.Cos(cameraAngle.Y * rads));
+
+            Matrix4 lookAt = Matrix4.LookAt(eyePos, new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 1.0f, 0.0f));
+            GL.LoadMatrix(Matrix4.Transpose(lookAt).Matrix);
+
+            DrawOrigin();
+
+            GL.Color3(1f, 1f, 1f);
+            plane.Render(4f);
+
+            Point closest = Collisions.PlaneCollision.ClosestPoint(plane, point);
+            float distance = Collisions.PlaneCollision.DistanceFromPlane(point, plane);
+            Vector3 vec = point.ToVector() - plane.Normal * distance;
+
+            GL.Color3(0f, 0f, 1f);
+            GL.Begin(PrimitiveType.Lines);
+            GL.Vertex3(point.X, point.Y, point.Z);
+            GL.Vertex3(vec.X, vec.Y, vec.Z);
+            GL.End();
+
+            GL.Color3(1f, 0f, 1f);
+            GL.Begin(PrimitiveType.Lines);
+            GL.Vertex3(closest.X, closest.Y, closest.Z);
+            GL.Vertex3(closest.X + plane.Normal.Z, closest.Y + plane.Normal.Y, closest.Z + plane.Normal.Z);
+            GL.End();
+
+            GL.Color3(1f, 0f, 0f);
+            point.Render();
+
+            GL.Color3(0, 1f, 0f);
+            closest.Render();
+        }
+
+        public override void Update(float deltaTime) {
+            cameraAngle.X += 45.0f * deltaTime;
+        }
+
+        protected void DrawOrigin() {
+            GL.Begin(PrimitiveType.Lines);
+            GL.Color3(1f, 0f, 0f);
+            GL.Vertex3(0f, 0f, 0f);
+            GL.Vertex3(1f, 0f, 0f);
+            GL.Color3(0f, 1f, 0f);
+            GL.Vertex3(0f, 0f, 0f);
+            GL.Vertex3(0f, 1f, 0f);
+            GL.Color3(0f, 0f, 1f);
+            GL.Vertex3(0f, 0f, 0f);
+            GL.Vertex3(0f, 0f, 1f);
+            GL.End();
+        }
+
+        public override void Resize(int width, int height) {
+            GL.Viewport(0, 0, width, height);
+            GL.MatrixMode(MatrixMode.Projection);
+            float aspect = (float)width / (float)height;
+            Matrix4 perspective = Matrix4.Perspective(60, aspect, 0.01f, 1000.0f);
+            GL.LoadMatrix(Matrix4.Transpose(perspective).Matrix);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+        }
+    }
+}
