7558274a4f51a45b629361605cf778d4c3723cc3
mszauer <mszauer@mail.usf.edu>
2016-06-11 16:22:05 -0400

    added pointonray case, refactored application abse class

--- CollisionDetectionSelector/Application.cs
+++ CollisionDetectionSelector/Application.cs
@@ -3,40 +3,85 @@ using OpenTK;
 using System.Drawing;
 using OpenTK.Graphics.OpenGL;
 
-    class Application {
+    public class Application {
         public static Application Instance {
             get; private set;
         }
         public OpenTK.GameWindow Window = null;
+        private Math_Implementation.Vector3 _baseCameraAngle = new Math_Implementation.Vector3(120.0f, -10.0f, 20.0f);
+        private float _baseRads = (float)(System.Math.PI / 180.0f);
 
         public Application() {
             Instance = this;
         }
 
-        //logic driving functions that are overwritten
-        #region InheritableLogic
-        public virtual void Initialize(int width,int height) {
+        #region CommonMethods
+        public void LogError(string error) {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Error: " + error);
+            Console.ResetColor();
+        }
+
+        public void DrawOrigin() {
+            GL.Begin(PrimitiveType.Lines);
+            GL.Color3(1f, 0f, 0f);
+            GL.Vertex3(0f, 0f, 0f);
+            GL.Vertex3(1f, 0f, 0f);
+            GL.Color3(0f, 1f, 0f);
+            GL.Vertex3(0f, 0f, 0f);
+            GL.Vertex3(0f, 1f, 0f);
+            GL.Color3(0f, 0f, 1f);
+            GL.Vertex3(0f, 0f, 0f);
+            GL.Vertex3(0f, 0f, 1f);
+            GL.End();
+        }
+        #endregion
+
+    //logic driving functions that are overwritten
+    #region InheritableLogic
+    public virtual void Initialize(int width,int height) {
 
         }
-        public virtual void Resize(int width,int height) {
 
-        }
         public virtual void Update(float dTime) {
-
+            _baseCameraAngle.X += 45.0f * dTime;
         }
         public virtual void Render() {
 
         }
         public virtual void Shutdown() {
+            // Again, Vector3 and Matrix4 must be fully scoped, because
+            // the OpenTK namespace contains both classes already
+            Math_Implementation.Vector3 eyePos = new Math_Implementation.Vector3();
+            eyePos.X = _baseCameraAngle.Z * -(float)System.Math.Sin(_baseCameraAngle.X * _baseRads * (float)System.Math.Cos(_baseCameraAngle.Y * _baseRads));
+            eyePos.Y = _baseCameraAngle.Z * -(float)System.Math.Sin(_baseCameraAngle.Y * _baseRads);
+            eyePos.Z = -_baseCameraAngle.Z * (float)System.Math.Cos(_baseCameraAngle.X * _baseRads * (float)System.Math.Cos(_baseCameraAngle.Y * _baseRads));
 
+            Math_Implementation.Matrix4 lookAt =
+                Math_Implementation.Matrix4.LookAt(eyePos,
+                new Math_Implementation.Vector3(0.0f, 0.0f, 0.0f),
+                new Math_Implementation.Vector3(0.0f, 1.0f, 0.0f));
+
+            GL.LoadMatrix(Math_Implementation.Matrix4.Transpose(lookAt).Matrix);
         }
+    // Has a default implementation, making the need to override in child
+    // classes purley optional
+    public virtual void Resize(int width, int height) {
+            GL.Viewport(0, 0, width, height);
+            GL.MatrixMode(MatrixMode.Projection);
+            float aspect = (float)width / (float)height;
+            Math_Implementation.Matrix4 perspective =
+                Math_Implementation.Matrix4.Perspective(60, aspect, 0.01f, 1000.0f);
+            GL.LoadMatrix(Math_Implementation.Matrix4.Transpose(perspective).Matrix);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+        }
+    #endregion
 
-        #endregion
+    #region EntryPoint
 
-        #region EntryPoint
-
-        //entry point of application, not overwritten
-        public virtual void Main(string[] args) {
+    //entry point of application, not overwritten
+    public virtual void Main(string[] args) {
             Window = new OpenTK.GameWindow();
             Window.Load += new EventHandler<EventArgs>(OpenTKInitialize);
             Window.UpdateFrame += new EventHandler<FrameEventArgs>(OpenTKUpdate);
